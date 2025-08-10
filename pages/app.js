e94916f5ca1ef9ed6a9166607096cc9569bd2173
Tkiezview <gossiptele@gmail.com>
2025-08-10 20:48:58 +0200

    Create app.js

---
+++ pages/app.js
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {});
+    }
+  }, []);
+  return <Component {...pageProps} />;
+}
